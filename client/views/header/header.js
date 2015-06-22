99ee273570f162a1138dfd8d7b8e27b9c2f8358b
Aldo Funes <aldo@funes.mx>
2015-06-21 19:42:33 -0500

    Update some packages

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,33 +1,5 @@
 Template['header'].onRendered(function(){
 
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     * We target all a tags inside the nav, and apply the scrollto.js to it.
-     */
-    console.log(window.location.pathname);
-    if ( window.location.pathname == '/' ) {
-        $("#navbar-main a, a.navbar-brand").click(function (evn) {
-            evn.preventDefault();
-            $.scrollTo(this.hash);
-        });
-    }
-
-    firstSectionId = "the-couple";
-    firstSectionElement = document.getElementById(firstSectionId);
-
-    $(window).scroll(function () {
-
-        // Get Scroll Position
-        var wScroll = $(this).scrollTop();
-
-        // fade in .navbar
-        if ( wScroll >= firstSectionElement.offsetTop ) {
-            $('.navbar').fadeIn();
-        } else {
-            $('.navbar').fadeOut();
-        }
-    });
-
 });
 
 
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,4 +1,30 @@
 Template['home'].onRendered(function(){
+    /**
+     * This part causes smooth scrolling using scrollto.js
+     * We target all a tags inside the nav, and apply the scrollto.js to it.
+     */
+    if ( window.location.pathname == '/' ) {
+        $("#navbar-main a, a.navbar-brand").click(function (evn) {
+            evn.preventDefault();
+            $.scrollTo(this.hash);
+        });
+    }
+
+    firstSectionId = "the-couple";
+    firstSectionElement = document.getElementById(firstSectionId);
+
+    $(window).scroll(function () {
+
+        // Get Scroll Position
+        var wScroll = $(this).scrollTop();
+
+        // fade in .navbar
+        if ( wScroll >= firstSectionElement.offsetTop ) {
+            $('.navbar').fadeIn();
+        } else {
+            $('.navbar').fadeOut();
+        }
+    });
 });
 
 Template['home'].helpers({
