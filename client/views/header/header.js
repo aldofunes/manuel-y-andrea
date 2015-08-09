810f80f9f164feed2ea8eeb21286754224c0195c
Aldo Funes <aldo@funes.mx>
2015-08-09 00:16:10 -0500

    Setup Kadira and GoSquared

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,20 +1,9 @@
 Template['header'].onRendered(function(){
-
 });
 
 
 Template['header'].helpers({
-    scrollNav: function () {
-        if ( window.location.pathname == '/' ) {
-            return "scroll-nav";
-        }
-    }
 });
 
 Template['header'].events({
-    'click .scroll-nav': function (event) {
-        //event.preventDefault();
-        console.log('I have been clicked');
-        //$.scrollTo(this.id);
-    }
 });
